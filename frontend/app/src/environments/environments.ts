3d27ed832178a720921f639c52b9097dde31bdb3
essayeswajih <wajihsayes@gmail.com>
2025-07-21 22:58:45 +0100

    fixx

--- frontend/app/src/environments/environments.ts
+++ frontend/app/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,        // Set to `true` for production environment
-  apiUrl: 'http://51.210.177.185/api',  // Your FastAPI backend URL during development
+  apiUrl: 'https://51.210.177.185/api',  // Your FastAPI backend URL during development
 };
