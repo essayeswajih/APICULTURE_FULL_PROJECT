e6fc713b1f69d70f1c68be629785a4db97c69a4c
essayeswajih <wajihsayes@gmail.com>
2025-07-25 10:23:38 +0100

    ngx toastr

--- frontend/app/src/app/toastr-demo/toastr-demo.ts
+++ frontend/app/src/app/toastr-demo/toastr-demo.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToasterManager } from '../services/toaster-manager';
 
 @Component({
   selector: 'app-toastr-demo',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './toastr-demo.scss'
 })
 export class ToastrDemo {
+constructor(private toaster: ToasterManager) {}
 
+  showSuccess() {
+    this.toaster.success('Operation successful!', 'Success');
+  }
+
+  showError() {
+    this.toaster.error('Something went wrong!', 'Error');
+  }
+
+  showInfo() {
+    this.toaster.info('This is an info message.', 'Info');
+  }
+
+  showWarning() {
+    this.toaster.warning('Be careful!', 'Warning');
+  }
+
+  clear() {
+    this.toaster.clear();
+  }
 }
