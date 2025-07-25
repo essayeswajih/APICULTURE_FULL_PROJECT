549edf2e86826cb54adef77b7b76846310bdf2d4
essayeswajih <wajihsayes@gmail.com>
2025-07-25 10:34:40 +0100

    ngx toastr

--- frontend/app/src/app/app.ts
+++ frontend/app/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Home } from "./pages/home/home";
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
+import { ToastrDemo } from "./toastr-demo/toastr-demo";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, ToastrDemo],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
