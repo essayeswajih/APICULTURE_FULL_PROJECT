1a8ae914ebb15783f100541407fec54bdcfa72e7
essayeswajih <wajihsayes@gmail.com>
2025-07-29 23:22:04 +0100

    subscribe and contact

--- backend/app/controller/imagesUpload.py
+++ backend/app/controller/imagesUpload.py
@@ -9,7 +9,7 @@ router = APIRouter()
 
 UPLOAD_DIR = "uploads/"
 BASE_STATIC_URL = "https://apiculturegalai.tn/uploads"  # ✅ DO NOT use /api here
-
+ge_tUrl = "https://apiculturegalai.tn/api/uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
@@ -32,7 +32,7 @@ def list_uploaded_images():
     try:
         files = os.listdir(UPLOAD_DIR)
         image_files = [
-            f"https://apiculturegalai.tn/api/{filename}"
+            f"{BASE_STATIC_URL}/{filename}"
             for filename in files
             if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp'))
         ]
