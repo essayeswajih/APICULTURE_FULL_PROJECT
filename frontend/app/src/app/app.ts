8450e27e1214ee083d6b3a4923f225937b4655ef
essayeswajih <wajihsayes@gmail.com>
2026-03-20 11:25:18 +0100

    trying to to set sub category

--- frontend/app/src/app/app.ts
+++ frontend/app/src/app/app.ts
@@ -90,6 +90,7 @@ menuItems = [
   }
 ];
   // Disable right click
+  /*
   @HostListener('document:contextmenu', ['$event'])
   onRightClick(event: MouseEvent) {
     event.preventDefault();
@@ -114,4 +115,5 @@ menuItems = [
       event.preventDefault();
     }//
   }
+    */
 }
