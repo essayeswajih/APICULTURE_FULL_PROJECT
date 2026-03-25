d82e965ab1aa091e9e3e995f733a18d515e14318
essayeswajih <wajihsayes@gmail.com>
2026-03-25 12:53:59 +0100

    more analytics

--- frontend/app/src/app/pages/story-management/story-management.ts
+++ frontend/app/src/app/pages/story-management/story-management.ts
@@ -149,7 +149,7 @@ export class StoryManagement implements OnInit {
 
     switch (this.newStory.platform) {
       case 'youtube':
-        url = `https://www.youtube.com/embed/${this.video_id}?autoplay=1&controls=0&rel=0`;
+        url = `https://www.youtube.com/embed/${this.video_id}?autoplay=1`;
         this.newStory.url = url; // Update URL for YouTube to ensure correct embedding
         this.cdRef.markForCheck();
         break;
